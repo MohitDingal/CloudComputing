b2a829cb22ac2023b0b76702f2df97e8ee6f72a6
MohitDingal <69381852+MohitDingal@users.noreply.github.com>
2022-03-31 12:11:29 +0530

    Update render.js

--- server/services/render.js
+++ server/services/render.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
 
-
+const PORT2 = process.env.PORT || 3000
 exports.homeRoutes = (req, res) => {
     // Make a get request to /api/users
-    axios.get('http://localhost:3000/api/users')
+    axios.get(`http://localhost:${PORT2}/api/users`)
         .then(function(response){
             res.render('index', { users : response.data });
         })
@@ -19,7 +19,7 @@ exports.add_user = (req, res) =>{
 }
 
 exports.update_user = (req, res) =>{
-    axios.get('http://localhost:3000/api/users', { params : { id : req.query.id }})
+    axios.get(`http://localhost:${PORT2}/api/users`, { params : { id : req.query.id }})
         .then(function(userdata){
             res.render("update_user", { user : userdata.data})
         })
