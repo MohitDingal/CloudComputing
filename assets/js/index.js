eaf5af536c3efd8b2e962a213d2790ff70c4f83e
MohitDingal <69381852+MohitDingal@users.noreply.github.com>
2022-04-03 15:02:41 +0530

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,7 +14,7 @@ $("#update_user").submit(function(event){
 
 
     var request = {
-        "url" : `http://localhost:3000/api/users/${data.id}`,
+        "url" : `http://0.0.0.0/api/users/${data.id}`,
         "method" : "PUT",
         "data" : data
     }
@@ -31,7 +31,7 @@ if(window.location.pathname == "/"){
         var id = $(this).attr("data-id")
 
         var request = {
-            "url" : `http://localhost:3000/api/users/${id}`,
+            "url" : `http://0.0.0.0/api/users/${id}`,
             "method" : "DELETE"
         }
 
